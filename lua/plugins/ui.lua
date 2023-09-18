2be17662545795b46b3268c98dbf4019fc17ba1e
Anant Narayan <anantnrg@gmail.com>
2023-09-18 14:55:11 +0530

    added alpha.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -52,4 +52,36 @@ return {
 			},
 		},
 	},
+	{
+		"goolord/alpha-nvim",
+		config = function()
+			local alpha = require("alpha")
+			local dashboard = require("alpha.themes.dashboard")
+
+			dashboard.section.header.val = {
+				"███▄▄▄▄      ▄████████  ▄██████▄   ▄█    █▄   ▄█    ▄▄▄▄███▄▄▄▄   ",
+				"███▀▀▀██▄   ███    ███ ███    ███ ███    ███ ███  ▄██▀▀▀███▀▀▀██▄ ",
+				"███   ███   ███    █▀  ███    ███ ███    ███ ███▌ ███   ███   ███ ",
+				"███   ███  ▄███▄▄▄     ███    ███ ███    ███ ███▌ ███   ███   ███",
+				"███   ███ ▀▀███▀▀▀     ███    ███ ███    ███ ███▌ ███   ███   ███ ",
+				"███   ███   ███    █▄  ███    ███ ███    ███ ███  ███   ███   ███",
+				"███   ███   ███    ███ ███    ███ ███    ███ ███  ███   ███   ███ ",
+				" ▀█   █▀    ██████████  ▀██████▀   ▀██████▀  █▀    ▀█   ███   █▀",
+			}
+
+			dashboard.section.buttons.val = {
+				dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+				dashboard.button("f", "  > Find file", ":cd $HOME/ | Telescope find_files<CR>"),
+				dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+				dashboard.button(
+					"s",
+					"  > Settings",
+					":e $HOME/.config/nvim/init.lua | :cd %:p:h | wincmd k | pwd<CR>"
+				),
+				dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+			}
+
+			alpha.setup(dashboard.opts)
+		end,
+	},
 }
