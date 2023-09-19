1a281241ef6c8c43095c8257fcd7676e7cb211d2
Anant Narayan <anantnrg@gmail.com>
2023-09-19 17:27:32 +0530

    fixed error. should probably test before committing

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -6,24 +6,24 @@ local builtin = require("telescope.builtin")
 -- Makes it easier to map stuff.
 local map = vim.keymap.set
 
-set("n", ";", ":")
+map("n", ";", ":")
 
-set("i", "<C-j>", "<C-o>gj")
-set("i", "<C-k>", "<C-o>gk")
-set("i", "<C-h>", "<Left>")
-set("i", "<C-l>", "<Right>")
+map("i", "<C-j>", "<C-o>gj")
+map("i", "<C-k>", "<C-o>gk")
+map("i", "<C-h>", "<Left>")
+map("i", "<C-l>", "<Right>")
 
-set("n", "j", "gj")
-set("n", "k", "gk")
+map("n", "j", "gj")
+map("n", "k", "gk")
 
 -- Session keybinds
-set("n", "<leader>sl", "<cmd> Telescope persisted <CR>")
-set("n", "<leader>ss", "<cmd> SessionSave <CR>")
-set("n", "<leader>sd", "<cmd> SessionDelete <CR>")
+map("n", "<leader>sl", "<cmd> Telescope persisted <CR>")
+map("n", "<leader>ss", "<cmd> SessionSave <CR>")
+map("n", "<leader>sd", "<cmd> SessionDelete <CR>")
 
 -- Telescope bindings
-set("n", "<leader>ff", builtin.find_files, {})
-set("n", "<leader>fg", builtin.live_grep, {})
-set("n", "<leader>fb", builtin.buffers, {})
-set("n", "<leader>fh", builtin.help_tags, {})
-set("n", "<leader>th", builtin.colorscheme, {})
+map("n", "<leader>ff", builtin.find_files, {})
+map("n", "<leader>fg", builtin.live_grep, {})
+map("n", "<leader>fb", builtin.buffers, {})
+map("n", "<leader>fh", builtin.help_tags, {})
+map("n", "<leader>th", builtin.colorscheme, {})
