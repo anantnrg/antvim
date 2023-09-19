f6852f40e84aa60dd5a1a09e7431d6edb07539eb
Anant Narayan <anantnrg@gmail.com>
2023-09-19 17:24:22 +0530

    refactored mappings.lua

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -3,24 +3,27 @@
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", ";", ":")
+-- Makes it easier to map stuff.
+local map = vim.keymap.set
 
-vim.keymap.set("i", "<C-j>", "<C-o>gj")
-vim.keymap.set("i", "<C-k>", "<C-o>gk")
-vim.keymap.set("i", "<C-h>", "<Left>")
-vim.keymap.set("i", "<C-l>", "<Right>")
+set("n", ";", ":")
 
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
+set("i", "<C-j>", "<C-o>gj")
+set("i", "<C-k>", "<C-o>gk")
+set("i", "<C-h>", "<Left>")
+set("i", "<C-l>", "<Right>")
+
+set("n", "j", "gj")
+set("n", "k", "gk")
 
 -- Session keybinds
-vim.keymap.set("n", "<leader>sl", "<cmd> Telescope persisted <CR>")
-vim.keymap.set("n", "<leader>ss", "<cmd> SessionSave <CR>")
-vim.keymap.set("n", "<leader>sd", "<cmd> SessionDelete <CR>")
+set("n", "<leader>sl", "<cmd> Telescope persisted <CR>")
+set("n", "<leader>ss", "<cmd> SessionSave <CR>")
+set("n", "<leader>sd", "<cmd> SessionDelete <CR>")
 
 -- Telescope bindings
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>th", builtin.colorscheme, {})
+set("n", "<leader>ff", builtin.find_files, {})
+set("n", "<leader>fg", builtin.live_grep, {})
+set("n", "<leader>fb", builtin.buffers, {})
+set("n", "<leader>fh", builtin.help_tags, {})
+set("n", "<leader>th", builtin.colorscheme, {})
