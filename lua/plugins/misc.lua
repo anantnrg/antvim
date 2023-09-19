e831a3438938e3a90ede51128b41c03c7636da83
Anant Narayan <anantnrg@gmail.com>
2023-09-19 14:57:23 +0530

    configure telescope. thx siduck

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -2,8 +2,8 @@ return {
 	{
 		"norcalli/nvim-colorizer.lua",
 		config = function()
-			require("colorizer").setup();
-		end
+			require("colorizer").setup()
+		end,
 	},
 	{
 		"echasnovski/mini.nvim",
@@ -12,21 +12,23 @@ return {
 	{
 		"rrethy/vim-illuminate",
 		config = function()
-			require('illuminate').configure({
-			    providers = {
-			        'lsp',
-			        'treesitter',
-			        'regex',
-			    },
-			    delay = 100,
+			require("illuminate").configure({
+				providers = {
+					"lsp",
+					"treesitter",
+					"regex",
+				},
+				delay = 100,
 				under_cursor = true,
-			    large_file_cutoff = nil,
+				large_file_cutoff = nil,
 				large_file_overrides = nil,
 				min_count_to_highlight = 1,
-				should_enable = function(bufnr) return true end,
-			    case_insensitive_regex = false,
+				should_enable = function(bufnr)
+					return true
+				end,
+				case_insensitive_regex = false,
 			})
-		end
+		end,
 	},
 	{
 		"folke/which-key.nvim",
@@ -45,5 +47,33 @@ return {
 		"kylechui/nvim-surround",
 		event = "VeryLazy",
 		config = true,
+	},
+	{
+		"m4xshen/autoclose.nvim",
+		config = function()
+			require("autoclose").setup({
+				keys = {
+					["("] = { escape = false, close = true, pair = "()" },
+					["["] = { escape = false, close = true, pair = "[]" },
+					["{"] = { escape = false, close = true, pair = "{}" },
+
+					[">"] = { escape = true, close = false, pair = "<>" },
+					[")"] = { escape = true, close = false, pair = "()" },
+					["]"] = { escape = true, close = false, pair = "[]" },
+					["}"] = { escape = true, close = false, pair = "{}" },
+
+					['"'] = { escape = true, close = true, pair = '""' },
+					["'"] = { escape = true, close = true, pair = "''" },
+					["`"] = { escape = true, close = true, pair = "``" },
+				},
+				options = {
+					disabled_filetypes = { "text" },
+					disable_when_touch = false,
+					touch_regex = "[%w(%[{]",
+					pair_spaces = false,
+					auto_indent = true,
+				},
+			})
+		end,
 	},
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,17 +19,45 @@ return {
 
 			telescope.setup({
 				defaults = {
-					layout_strategy = "flex",
+					vimgrep_arguments = {
+						"rg",
+						"-L",
+						"--color=always",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
+					prompt_prefix = "   ",
+					selection_caret = "  ",
+					entry_prefix = "  ",
+					initial_mode = "insert",
+					selection_strategy = "reset",
+					sorting_strategy = "ascending",
+					layout_strategy = "horizontal",
 					layout_config = {
-						flex = { flip_columns = 200 },
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+							results_width = 0.8,
+						},
 					},
+					file_sorter = require("telescope.sorters").get_fuzzy_file,
+					file_ignore_patterns = { "node_modules" },
+					generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+					path_display = { "truncate" },
+					winblend = 0,
+					border = {},
+					borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					color_devicons = true,
+					set_env = { ["COLORTERM"] = "truecolor" },
+					file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+					grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+					qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+					buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
 					mappings = {
-						i = { ["<esc>"] = require("telescope.actions").close },
-					},
-				},
-				extensions = {
-					ast_grep = {
-						command = { "sg", "--json=stream", "-p" },
+						n = { ["q"] = require("telescope.actions").close },
 					},
 				},
 			})
