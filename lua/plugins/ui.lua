86d5ce762424267ff038f3c9d104d78d006211f6
Anant Narayan <anantnrg@gmail.com>
2023-09-21 08:53:03 +0530

    lualine ftw

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -183,4 +183,29 @@ return {
 			})
 		end,
 	},
+	{
+		"nvim-lualine/lualine.nvim",
+		config = function()
+			require("lualine").setup({
+				options = {
+					icons_enabled = true,
+					theme = "gruvbox-material",
+					component_separators = "|",
+					section_separators = { left = "", right = "" },
+				},
+				sections = {
+					lualine_a = {
+						{ "mode", separator = { left = "" }, right_padding = 2 },
+					},
+					lualine_b = { "filename", "branch" },
+					lualine_c = { "fileformat" },
+					lualine_x = {},
+					lualine_y = { "filetype", "searchcount" },
+					lualine_z = {
+						{ "location", separator = { right = "" }, left_padding = 2 },
+					},
+				},
+			})
+		end,
+	},
 }
