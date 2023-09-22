71822331824e291c4dc310e0b4ccd83796b4f30c
Anant Narayan <anantnrg@gmail.com>
2023-09-22 21:33:52 +0530

    added readme

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -227,7 +227,7 @@ return {
 		"nvim-neo-tree/neo-tree.nvim",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
-			"kyazdani42/nvim-web-devicons",
+			"nvim-tree/nvim-web-devicons",
 			"MunifTanjim/nui.nvim",
 		},
 		cmd = "Neotree",
