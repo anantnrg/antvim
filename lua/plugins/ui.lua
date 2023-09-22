1506aefae205606776a6247c5b728b06cec34b5b
Anant Narayan <anantnrg@gmail.com>
2023-09-22 20:42:11 +0530

    removed neo-tree

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -208,14 +208,4 @@ return {
 			})
 		end,
 	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-			"MunifTanjim/nui.nvim",
-		},
-		config = function() end,
-	},
 }
