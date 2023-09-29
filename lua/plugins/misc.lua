c9de9cd6c7e6decdeb8ed53606a09293d89f39f1
Anant Narayan <anantnrg@gmail.com>
2023-09-29 17:49:16 +0530

    change to nvim-autopairs

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -49,32 +49,13 @@ return {
 		config = true,
 	},
 	{
-		"m4xshen/autoclose.nvim",
-		config = function()
-			require("autoclose").setup({
-				keys = {
-					["("] = { escape = false, close = true, pair = "()" },
-					["["] = { escape = false, close = true, pair = "[]" },
-					["{"] = { escape = false, close = true, pair = "{}" },
-
-					[">"] = { escape = true, close = false, pair = "<>" },
-					[")"] = { escape = true, close = false, pair = "()" },
-					["]"] = { escape = true, close = false, pair = "[]" },
-					["}"] = { escape = true, close = false, pair = "{}" },
-
-					['"'] = { escape = true, close = true, pair = '""' },
-					["'"] = { escape = true, close = true, pair = "''" },
-					["`"] = { escape = true, close = true, pair = "``" },
-				},
-				options = {
-					disabled_filetypes = { "text" },
-					disable_when_touch = false,
-					touch_regex = "[%w(%[{]",
-					pair_spaces = false,
-					auto_indent = true,
-				},
-			})
-		end,
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {
+			disable_filetype = { "TelescopePrompt" },
+			map_bs = true,
+			map_cr = true,
+		},
 	},
 	{
 		"numToStr/Comment.nvim",
