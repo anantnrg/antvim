a276f3961e956041cdbeda4bcec56a55935077ce
Anant Narayan <anantnrg@gmail.com>
2023-10-01 21:17:39 +0530

    did not touch emacs. just eww

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -37,4 +37,4 @@ map("n", "<S-Tab>", "<cmd> BufferLineCyclePrev<CR>")
 map("n", "<leader>x", "<cmd> bd<CR>")
 
 -- Terminal
-map("n", "<C-`>", "<cmd> ToggleTerm size=130 direction=vertical<CR>")
+map("n", "<C-`>", "<cmd> ToggleTerm size=120 direction=vertical<CR>")
